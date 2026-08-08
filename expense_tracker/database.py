77248a16353e3682f23dc4e42c997645b3a6f8a7
h-non <trythingsig@gmail.com>
2026-08-08 21:07:47 +0400

    Added a way to input expense data

--- expense_tracker/database.py
+++ expense_tracker/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, Float, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
-
+from datetime import datetime
 
 
 engine = create_engine("sqlite:///dataBase/expenses.db")
@@ -42,3 +42,36 @@ def add_expense(amount, category, description, exp_date):
      session.refresh(new_expense)
      session.close()
      return new_expense
+
+def inputexp():
+     
+     amnt = input("Enter the expended amount: ")
+     try:
+          amnt = float(amnt)
+     except ValueError:
+          print("Amount must be a number.")
+          return
+
+     catg = input("Enter the category of this expense: ")
+     desc = input("Enter the description of this expense: ")
+
+     dat = input("Enter the date of the expense (YYYY-MM-DD) or leave blank for todays' date: ")
+
+     if dat.strip() == "":
+          expdat = datetime.today().date()
+     else:
+          try:
+               expdat = datetime.strptime(dat,"%Y-%m-%d").date()
+          except ValueError:
+               print("Date must be inputed as Year-month-day format.")
+
+               return
+
+     expense = add_expense(amnt,catg,desc,expdat)
+     print(f"Added expense: {expense.amount}$ in {expense.category} on {expense.date}")
+
+
+
+
+
+
